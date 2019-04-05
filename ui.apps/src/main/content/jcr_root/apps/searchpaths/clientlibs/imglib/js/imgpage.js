b4ad13026b1cd82d4bd2c32dbad43ed6d443218b
Ramin <none@example.com>
2019-04-05 16:18:50 +0300

    task2 done libs

--- ui.apps/src/main/content/jcr_root/apps/searchpaths/clientlibs/imglib/js/imgpage.js
+++ ui.apps/src/main/content/jcr_root/apps/searchpaths/clientlibs/imglib/js/imgpage.js
@@ -1,11 +1,8 @@
 console.log("Buuya!");
 $(document).ready(function(){
- //$("#imgmy").on("onfocus",function(){console.log("it is did ");});
  var textForView=$("#titleImg").attr("titleImgAttr");
  $("#imgmy").attr("title", textForView);
  console.log("value=",textForView);
 
 });
-
-
 console.log("Buuya2!");
